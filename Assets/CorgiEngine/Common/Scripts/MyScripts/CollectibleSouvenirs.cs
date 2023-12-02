506087cab735a22ed12d54055aca2e42d50e69d7
Wesily-G <729009583@qq.com>
2023-12-02 16:55:21 -0500

    level update/ coins

---
+++ Assets/CorgiEngine/Common/Scripts/MyScripts/CollectibleSouvenirs.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeyboardWarrior
+{
+    public class CollectibleSouvenirs : MonoBehaviour
+    {
+        public float rotateSpeed = 30f;
+        public AudioSource audioSource;
+        private GameObject coinSprite;
+
+        void Start(){
+            coinSprite = transform.Find("Sprite").gameObject;
+        }
+        
+
+        void Update(){
+            this.transform.Rotate(new Vector3(0, rotateSpeed, 0) * Time.deltaTime, Space.Self);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject == PlayerManager.Instance.gameObject && coinSprite.active == true)
+            {
+                audioSource.Play(0);
+                coinSprite.active = false;
+            }
+        }
+
+    }
+}
