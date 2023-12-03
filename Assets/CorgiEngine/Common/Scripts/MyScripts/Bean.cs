34b0678eee9c008166ee794decc4d0dc21e56542
Mingbin Yang <yang.mingb@northeastern.edu>
2023-12-02 22:43:14 -0500

    bean

---
+++ Assets/CorgiEngine/Common/Scripts/MyScripts/Bean.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeyboardWarrior
+{
+    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(Collider2D))]
+    public class Bean : MonoBehaviour
+    {
+        public AudioClip audioOnCollect;
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag.ToLower() == "player")
+            {
+                if (BeanCounter.Instance)
+                {
+                    BeanCounter.Instance.GetBean();
+                }
+                if (audioOnCollect) GetComponent<AudioSource>().PlayOneShot(audioOnCollect);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
